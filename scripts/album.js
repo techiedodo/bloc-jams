a702c6e49d799fc4ad80c665d93ed2c9e1b02456
Luis Gaytan <gaytan_luis@msn.com>
2016-01-13 14:58:34 -0800

    ability to click through the different albums on the album page

--- scripts/album.js
+++ scripts/album.js
@@ -1,4 +1,21 @@
- // Example Album
+// My Album 
+var albumBeatles = {
+     name: 'Let It Be',
+     artist: 'Beatles',
+     label: 'Apple Records',
+     year: '1970',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { name: 'Two of Us', length: '3:36' },
+         { name: 'Dig a Pony', length: '3:54' },
+         { name: 'Across the Universe', length: '3:47' },
+         { name: 'I Me Mine', length: '2:26'},
+         { name: 'Dig It', length: '0:50'}
+     ]
+ }; 
+
+
+// Example Album
  var albumPicasso = {
      name: 'The Colors',
      artist: 'Pablo Picasso',
@@ -42,14 +59,14 @@ var createSongRow = function(songNumber, songName, songLength) {
      return template;
  };
 
-var setCurrentAlbum = function(album) {
      // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+var setCurrentAlbum = function(album) {
      // #2
      albumTitle.firstChild.nodeValue = album.name;
      albumArtist.firstChild.nodeValue = album.artist;
@@ -67,4 +84,15 @@ var setCurrentAlbum = function(album) {
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+     
+     var albums = [albumBeatles, albumPicasso, albumMarconi];
+     var index = 1;
+     
+     albumImage.addEventListener("click", function(event) {
+         setCurrentAlbum(albums[index]);
+         index++;
+         if (index == albums.length) {
+            index = 0;
+         }
+     });
  };
