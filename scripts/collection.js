de048077badcc845d765ccfbf07c3a67f446b976
Luis Gaytan <gaytan_luis@msn.com>
2016-01-19 12:40:51 -0800

    refactored collection and album viwews with jQuery

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,5 +1,6 @@
- var collectionItemTemplate =
-     '<div class="collection-album-container column fourth">'
+var buildCollectionItemTemplate = function() {
+    var template = 
+    '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
    + '    <p>'
@@ -13,15 +14,15 @@
    + '  </div>'
    + '</div>'
    ;
+    return $(template);
+};
 
- window.onload = function() {
-     // #1
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
-     // #2
-     collectionContainer.innerHTML = '';
- 
-     // #3
-     for (var i = 0; i < 5; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
-     }
- }
+$(window).load(function() {
+    var $collectionContainer = $('.album-covers');
+    $collectionContainer.empty();
+    
+    for (var i=0; i < 12; i++) {
+        var $newThumbnail = buildCollectionItemTemplate();
+        $collectionContainer.append($newThumbnail);
+    }
+});
